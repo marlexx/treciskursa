e8c367d6f2cf8e8c2ff783a0a5e7fe2764598a03
marlexx <122455888+marlexx@users.noreply.github.com>
2024-02-03 13:42:46 +0100

    first commit

---
+++ Meme generator/src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./assets/Header"
+import Meme from "./assets/Meme"
+import "./App.css"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
